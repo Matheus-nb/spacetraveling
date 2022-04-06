2305588dced31c2bcec8884b26734ef96ec39f0b
Matheus Lascalla <matheusalg.nb@gmail.com>
2022-04-06 12:32:15 -0400

    Starter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -24,13 +28,70 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {
+            postsPagination.results.map(post => (
+              <Link href={`/posts/${post.uid}`}>
+                <a key={post.uid} >
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <div>
+                    <time>{post.first_publication_date}</time>
+                    <p>{post.data.author}</p>
+                  </div>
+                </a>
+              </Link>
+            ))
+          }
+        </div>
+      </main>
+    </>
+  )
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 5,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      }
+    }
+  })
+
+  const postsPagination = {
+    results: posts,
+    next_page: postsResponse.next_page,
+  }
+
+
+  return {
+    props: {
+      postsPagination,
+    }
+  }
+};
